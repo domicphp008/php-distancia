c914793a32ee9f69c6c693a119a42f0078394499
Domicio Jose <testedomicio@hotmail.com>
2016-04-17 21:49:47 -0300

    Aprendendo operadores

---
+++ operadores-matematicos.php
@@ -0,0 +1,44 @@
+<?php
+
+$var1 = 10;
+$var2 = 20;
+$var3 = 30;
+
+//adiçao
+//echo $var1 + $var2;
+
+//subtracao
+//echo $var1 - $var2;
+
+//multiplicacao
+//echo $var1 * $var2;
+
+//divisao
+//echo $var1 / $var2;
+
+//modulo (resato da divisao)
+//echo $var1 % 3;
+//echo '<br>';
+//echo $var2 % 3;
+//echo '<br>';
+//echo $var3 % 3;
+//echo '<br>';
+
+/*
+ * ordem das operações
+echo $var1 + $var2 * $var3;
+echo '<br>';
+echo ($var1 + $var2) * $var3;
+ */
+
+/* incremento e decremento
+$var1 = $var1 +1;
+$var1++;$var1++;$var1++;$var1++;$var1--;$var1--;
+echo $var1;
+ */
+
+$var1 = $var1 * 10;
+$var1 += 10;
+$aviso = '';
+$aviso .= 'A minha mensagem de aviso';
+$aviso = $aviso . 'A minha mensagem de aviso';
